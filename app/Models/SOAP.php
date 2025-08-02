c50351e9434b61e38d7d2bbdb8241ecda5ed9d35
David Mendoza <davidmendozaofficial@gmail.com>
2025-08-02 15:59:28 +0800

    soap saving part 1
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ app/Models/SOAP.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SOAP extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'soaps';
+
+    protected $fillable = [
+        'clinic_id',
+        'patient_id',
+        's_type_of_visit',
+        's_chief_complaint',
+        's_history_of_present_illness',
+        'o_systolic',
+        'o_diastolic',
+        'o_pulse',
+        'o_pulse_type',
+        'o_temperature',
+        'o_temperature_unit',
+        'o_temperature_location',
+        'o_respiration_rate',
+        'o_respiration_type',
+        'o_weight',
+        'o_weight_unit',
+        'o_height',
+        'o_height_unit',
+        'o_o2_sat',
+        'o_drawing',
+        'o_physical_examination',
+        'a_previous_diagnosis',
+        'a_diagnosis',
+        'p_laboratory_requests',
+        'p_imaging_requests',
+        'p_diagnosis_care_plan',
+        'p_previous_medication',
+        'p_therapeutic_care_plan',
+        'p_doctors_note',
+        'p_files'
+    ];
+    
+    protected $casts = [
+        'created_at' => "datetime", 
+        'updated_at' => "datetime", 
+        'deleted_at' => "datetime"
+    ];
+}
---
+++ database/migrations/2025_08_01_180327_create_s_o_a_p_s_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('soaps', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger("clinic_id")->nullable();
+            $table->unsignedInteger("patient_id")->nullable();
+
+            $table->string('s_type_of_visit')->nullable();
+            $table->string('s_chief_complaint')->nullable();
+            $table->string('s_history_of_present_illness')->nullable();
+            
+            $table->string('o_systolic')->nullable();
+            $table->string('o_diastolic')->nullable();
+            $table->string('o_pulse')->nullable();
+            $table->string('o_pulse_type')->nullable();
+            $table->string('o_temperature')->nullable();
+            $table->string('o_temperature_unit')->nullable();
+            $table->string('o_temperature_location')->nullable();
+            $table->string('o_respiration_rate')->nullable();
+            $table->string('o_respiration_type')->nullable();
+            $table->string('o_weight')->nullable();
+            $table->string('o_weight_unit')->nullable();
+            $table->string('o_height')->nullable();
+            $table->string('o_height_unit')->nullable();
+            $table->string('o_o2_sat')->nullable();
+            $table->string('o_drawing')->nullable();
+            $table->text('o_physical_examination')->nullable();
+
+            $table->string('a_previous_diagnosis')->nullable();
+            $table->string('a_diagnosis')->nullable();
+
+            $table->string('p_laboratory_requests')->nullable();
+            $table->string('p_imaging_requests')->nullable();
+            $table->string('p_diagnosis_care_plan')->nullable();
+            $table->string('p_previous_medication')->nullable();
+            $table->string('p_therapeutic_care_plan')->nullable();
+            $table->string('p_doctors_note')->nullable();
+
+            $table->text('p_files')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('soaps');
+    }
+};
