65a0baf5c7b41f7006bc395cc535ac02e751eb96
David Mendoza <davidmendozaofficial@gmail.com>
2025-08-05 18:34:30 +0800

    soap part 2
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -157,6 +157,20 @@ Route::group([
             }
         );
 
+        // SOAP ROUTES
+        $cname = "soap";
+        Route::group([
+                'as' => "$cname.",
+                'prefix' => "$cname/"
+            ], function () use($cname){
+
+                Route::get("get/", ucfirst($cname) . "Controller@get")->name('get');
+                Route::post("store/", ucfirst($cname) . "Controller@store")->name('store');
+                Route::post("delete/", ucfirst($cname) . "Controller@delete")->name('delete');
+                Route::post("update/", ucfirst($cname) . "Controller@update")->name('update');
+            }
+        );
+
         // TEMPLATE ROUTES
         $cname = "question";
         Route::group([
